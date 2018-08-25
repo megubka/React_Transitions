38d0509c2885e38cb81572b98b191aef3f7ae2dc
Alimzhan Aigarayev <alimzhan.aigarayev@gmail.com>
2018-08-25 11:20:58 +0200

    modal_transition_native

--- src/Elements/Modal.js
+++ src/Elements/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Transition } from 'react-spring';
+import { Transition, animated } from 'react-spring';
 import { Portal, absolute } from 'Utilities'
 import Icon from './Icon'
 import { Card } from './Cards'
@@ -10,16 +10,21 @@ export default class Modal extends Component {
         const { children, toggle, on } = this.props;
         return (
             <Portal>
-                <Transition 
+                <Transition
+                    native 
+                    config={{
+                        tension: 280,
+                        friction: 60
+                    }}
                     from={{ opacity: 0 , bgOpacity: 0, y: -50 }} 
                     enter={{ opacity: 1 , bgOpacity: 0.7, y: 0 }} 
-                    leave={{ opacity: 0 , bgOpacity: 0, y: 50 }}
+                    leave={{ opacity: 0 , bgOpacity: 0, y: -50 }}
                 >
                     {on && (
                         (styles) => (
                             <ModalWrapper style={{ ...styles }}>
                                 <ModalCard style={{
-                                    transform: `translate3d(0, ${styles.y}px, 0)`,
+                                    transform: styles.y.interpolate( y => `translate3d(0, ${y}px, 0)` ),
                                     ...styles
                                 }}>
                                     <CloseButton onClick={toggle}>
@@ -27,7 +32,14 @@ export default class Modal extends Component {
                                     </CloseButton>
                                     <div>{children}</div>
                                 </ModalCard>
-                                <Background  style={{ opacity: styles.bgOpacity }} onClick={toggle} />
+                                <Background  
+                                    style={{ 
+                                        opacity: styles.bgOpacity.interpolate(
+                                            bgOpacity => bgOpacity
+                                        )
+                                    }} 
+                                    onClick={toggle} 
+                                />
                             </ModalWrapper>
                         )
                     )}
@@ -50,7 +62,9 @@ const ModalWindow = styled.div`
     position: relative;
 `;
 
-const ModalCard = Card.extend`
+const AnimCard = Card.withComponent(animated.div);
+
+const ModalCard = AnimCard.extend`
     position: relative;
     z-index: 1;
     min-width: 320px;
@@ -67,7 +81,7 @@ const CloseButton = styled.button`
     padding: 10px;
 `;
 
-const Background = styled.div`
+const Background = styled(animated.div)`
     ${absolute({})};
     width: 100%;
     height: 100%;
