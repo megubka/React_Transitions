f69e0d831c562cf0ba3be5afeb23437699f48428
Alimzhan Aigarayev <alimzhan.aigarayev@gmail.com>
2018-08-25 13:21:42 +0200

    drag module added and rest were removed

---
+++ src/Drag.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Gesture } from 'react-with-gesture';
+import { Spring, animated, interpolate } from 'react-spring';
+import { Card } from 'Elements';
+
+export default class Drag extends Component {
+  render() {
+    return (
+      <Gesture>
+          {({ down, xDelta }) => (
+
+            <Spring
+                to={{
+                    x: down ? xDelta : 0
+                }}
+                immediate={name => down && name === "x"}
+            >
+
+                {({x}) => (
+                    <Card style={{
+                        transform: `translateX(${x}px)`
+                    }}>
+                        <h1>Drag</h1>
+                    </Card>
+                )}
+
+            </Spring>
+          )}
+      </Gesture>
+    )
+  }
+}
